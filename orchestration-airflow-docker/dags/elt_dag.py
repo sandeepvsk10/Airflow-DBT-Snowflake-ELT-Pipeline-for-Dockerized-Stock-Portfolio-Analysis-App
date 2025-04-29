adb9e2e97ce82a8c3234a8f95d150416781764c1
Sandeepkumar, Vijaya Kumar <69055900+sandeepvsk10@users.noreply.github.com>
2025-04-29 18:52:01 -0400

    Update elt_dag.py

--- orchestration-airflow-docker/dags/elt_dag.py
+++ orchestration-airflow-docker/dags/elt_dag.py
@@ -4,34 +4,34 @@ from datetime import datetime
 
 with DAG('docker_apps_orchestration', start_date=datetime(2024, 1, 1), schedule_interval=None) as dag:
 
-    # Task to run app1 container
+   
     task_app1 = DockerOperator(
         task_id='run_app1',
-        image='app1-image',  # Docker image of app1
-        command='python app.py',  # Command to run in the container
-        auto_remove=True,  # Remove container once done
-        docker_url='unix://var/run/docker.sock',  # Connect to Docker
-        network_mode='airflow-net'  # Use the same network
+        image='app1-image',
+        command='python app.py',  
+        auto_remove=True,  
+        docker_url='unix://var/run/docker.sock', 
+        network_mode='airflow-net' 
     )
 
-    # Task to run app2 container
+    
     task_app2 = DockerOperator(
         task_id='run_app2',
-        image='app2-image',  # Docker image of app2
-        command='python app.py',  # Command to run in the container
-        auto_remove=True,  # Remove container once done
-        docker_url='unix://var/run/docker.sock',  # Connect to Docker
-        network_mode='airflow-net'  # Use the same network
+        image='app2-image',  
+        command='python app.py', 
+        auto_remove=True,  
+        docker_url='unix://var/run/docker.sock', 
+        network_mode='airflow-net' 
     )
 
     task_app2 = DockerOperator(
         task_id='run_app2',
-        image='app2-image',  # Docker image of app2
-        command='python app.py',  # Command to run in the container
-        auto_remove=True,  # Remove container once done
-        docker_url='unix://var/run/docker.sock',  # Connect to Docker
-        network_mode='airflow-net'  # Use the same network
+        image='app2-image', 
+        command='python app.py',
+        auto_remove=True,  
+        docker_url='unix://var/run/docker.sock',  
+        network_mode='airflow-net' 
     )
 
-    # Set the execution order of tasks
-    task_app1 >> task_app2  # app1 runs before app2
+    
+    task_app1 >> task_app2  
